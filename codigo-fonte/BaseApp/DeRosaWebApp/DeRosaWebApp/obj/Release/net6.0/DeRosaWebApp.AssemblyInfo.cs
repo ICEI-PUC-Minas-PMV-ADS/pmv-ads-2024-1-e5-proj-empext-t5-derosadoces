288a5c3bf734755a90ba69e97a9bada6eb3d127c
Fernando Alves <90225435+fernends@users.noreply.github.com>
2024-05-03 16:36:26 -0300

    Ícones
    Inserido ícones de lixeira, área de trabalho e carrinho de compras

--- codigo-fonte/BaseApp/DeRosaWebApp/DeRosaWebApp/obj/Release/net6.0/DeRosaWebApp.AssemblyInfo.cs
+++ codigo-fonte/BaseApp/DeRosaWebApp/DeRosaWebApp/obj/Release/net6.0/DeRosaWebApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeRosaWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9d1768b25c430867dba95f13d769c7c6c973f16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06c9bcdcf82c7f6308e5b9cefe846da2ed88efde")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeRosaWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeRosaWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
