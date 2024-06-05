a3a349e18ee9fc4f6436ada5f3fccbd62bd64e93
Fernando Alves <90225435+fernends@users.noreply.github.com>
2024-06-05 10:52:59 -0300

    Ajuste Meu Contato
    Ajuste no Layout da página Meus contatos

--- codigo-fonte/BaseApp/DeRosaWebApp/DeRosaWebApp/obj/Debug/net6.0/DeRosaWebApp.AssemblyInfo.cs
+++ codigo-fonte/BaseApp/DeRosaWebApp/DeRosaWebApp/obj/Debug/net6.0/DeRosaWebApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeRosaWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2854176656f93e3079cc3ad96523ed24818e478")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8436c12215747fe713cc1b393d5c571f862e403")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeRosaWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeRosaWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
