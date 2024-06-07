1b28ad27b5245929c54995c9057d79cad2873727
Fernando Alves <90225435+fernends@users.noreply.github.com>
2024-06-07 10:48:06 -0300

    Ajuste Layout pagamento
    Ajuste no layout na tela de confirmação de pagamento

--- codigo-fonte/BaseApp/DeRosaWebApp/DeRosaWebApp/obj/Debug/net6.0/DeRosaWebApp.AssemblyInfo.cs
+++ codigo-fonte/BaseApp/DeRosaWebApp/DeRosaWebApp/obj/Debug/net6.0/DeRosaWebApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeRosaWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8d835edbdcce23eed83c7dbf59ea28c8727d2d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ab3379f320e9d30e1fe858f733b710fd1edb458")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeRosaWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeRosaWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
