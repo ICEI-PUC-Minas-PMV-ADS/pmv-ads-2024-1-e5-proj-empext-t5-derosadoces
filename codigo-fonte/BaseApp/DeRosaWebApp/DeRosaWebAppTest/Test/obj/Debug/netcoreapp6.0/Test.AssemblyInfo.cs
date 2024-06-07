5ab3379f320e9d30e1fe858f733b710fd1edb458
Fernando Alves <90225435+fernends@users.noreply.github.com>
2024-06-07 10:22:36 -0300

    Atualização Layout Admin
    Atualização pontuais de botões na área admin

--- codigo-fonte/BaseApp/DeRosaWebApp/DeRosaWebAppTest/Test/obj/Debug/netcoreapp6.0/Test.AssemblyInfo.cs
+++ codigo-fonte/BaseApp/DeRosaWebApp/DeRosaWebAppTest/Test/obj/Debug/netcoreapp6.0/Test.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8d835edbdcce23eed83c7dbf59ea28c8727d2d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
